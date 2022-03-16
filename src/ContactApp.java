c2b5052d8d1e04d507e38467ba9dfe1614afd3d8
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-16 11:15:02 -0500

    created print menu method. Added loop for continuing program until user decides to quit

--- src/ContactApp.java
+++ src/ContactApp.java
@@ -14,16 +14,41 @@ public class ContactApp {
     private static final Input input = new Input();
 
     public static void main(String[] args) {
-        init();
+        contactList = init();
+        do {
+            printMenu();
+            switch (input.getInt("Enter an option (1, 2, 3, 4 or 5):")) {
+                case 1:
+                    System.out.println(contactList);
+                case 2:
+                    addContactToList();
+                case 3:
+                    searchContactByName();
+                case 4:
+                    deleteContact();
+                default:
+                    break;
+            }
+        } while(input.yesNo("Do you want to continue adding/editing contacts?"));
+        writeToContacts();
     }
 
-    private static void init() {
+    private static List<String> init() {
         try {
-            System.out.println(Files.readAllLines(contactsPath));
+            return Files.readAllLines(contactsPath);
         } catch (IOException e) {
             System.out.println("No file exists");
             e.printStackTrace();
         }
+        return null;
+    }
+
+    private static void printMenu(){
+        System.out.println("1. View contacts.\n" +
+                "2. Add a new contact.\n" +
+                "3. Search a contact by name.\n" +
+                "4. Delete an existing contact.\n" +
+                "5. Exit.\n");
     }
 
     private static void writeToContacts(){
